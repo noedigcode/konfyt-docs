184f8760daaac36f428d34e31559fd6400029a35
Gideon van der Kolf <noedigcode@gmail.com>
2023-12-15 18:22:23 +0200

    Bump version to 1.5.0 in conf.py.

--- conf.py
+++ conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Gideon van der Kolf'
 author = 'Gideon van der Kolf'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.2'
+release = '1.5.0'
 
 # The master toctree document.
 master_doc = 'index'
